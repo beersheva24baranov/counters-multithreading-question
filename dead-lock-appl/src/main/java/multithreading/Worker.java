a9d1073b757f371b4d8d7eb63848b34ae9c3c019
AlexanderBaranov <avbc@mail.ru>
2024-11-28 08:17:12 +0200

    HW # 44

--- dead-lock-appl/src/main/java/multithreading/Worker.java
+++ dead-lock-appl/src/main/java/multithreading/Worker.java
@@ -3,25 +3,25 @@ package multithreading;
 public class Worker extends Thread{
     private Object mutex1;
     private Object mutex2;
-    private String srcName1;
-    private String srcName2;
+    private String src1;
+    private String src2;
 
-    public Worker(String srcName1, Object mutex1, String srcName2, Object mutex2) {
+    public Worker(String src1, Object mutex1, String src2, Object mutex2) {
         this.mutex1 = mutex1;
         this.mutex2 = mutex2;
-        this.srcName1 = srcName1;
-        this.srcName2 = srcName2;
+        this.src1 = src1;
+        this.src2 = src2;
     }
 
     @Override
     public void run() {
         synchronized (mutex1) {
-            System.out.println("Locking %s by %s".formatted(srcName1, getName()));
+            System.out.println("Locking %s by %s".formatted(src1, getName()));
             synchronized (mutex2) {
-                System.out.println("Locking %s by %s".formatted(srcName2, getName()));
-                System.out.println("Unlocking %s by %s".formatted(srcName2, getName()));
+                System.out.println("Locking %s by %s".formatted(src2, getName()));
+                System.out.println("Unlocking %s by %s".formatted(src2, getName()));
             }
-            System.out.println("Unlocking %s by %s".formatted(srcName1, getName()));
+            System.out.println("Unlocking %s by %s".formatted(src1, getName()));
         } 
     }
 }
